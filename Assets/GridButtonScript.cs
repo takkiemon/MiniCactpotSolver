68e3c8d6f14a2df6cb9319a87da903cdb53ea2be
takkiemon <takkie91@hotmail.com>
2020-04-06 15:59:07 +0200

    add gridButtonScript

---
+++ Assets/GridButtonScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GridButtonScript : MonoBehaviour
+{
+    public Image buttonImage;
+    public Color defaultColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultColor = buttonImage.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HighLightIt()
+    {
+        buttonImage.color = new Color(178, 178, 178);
+    }
+
+    public void ReleaseHighLight()
+    {
+        buttonImage.color = defaultColor;
+    }
+}
